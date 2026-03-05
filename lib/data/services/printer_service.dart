b5e62f114f9ca60d8869f0bb8dd89381bdbde047
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-03-05 15:27:00 +0500

    Waiter Mobile: Added Font B support and layout styling

--- lib/data/services/printer_service.dart
+++ lib/data/services/printer_service.dart
@@ -92,16 +92,44 @@ class PrinterService {
     }
   }
 
+  PosStyles _getStyles(Map<String, dynamic> element, {bool defaultBold = false, PosAlign defaultAlign = PosAlign.center, PosTextSize defaultSize = PosTextSize.size1}) {
+    final props = element['props'] ?? {};
+    
+    PosAlign align = defaultAlign;
+    if (props['align'] == 'LEFT') align = PosAlign.left;
+    else if (props['align'] == 'CENTER') align = PosAlign.center;
+    else if (props['align'] == 'RIGHT') align = PosAlign.right;
+
+    bool bold = props['bold'] ?? defaultBold;
+    
+    PosTextSize size = defaultSize;
+    if (props['size'] == 'LARGE') size = PosTextSize.size2;
+    else if (props['size'] == 'XLARGE') size = PosTextSize.size3;
+    else if (props['size'] == 'NORMAL') size = PosTextSize.size1;
+
+    PosFontType font = PosFontType.fontA;
+    if (props['font'] == 'B') font = PosFontType.fontB;
+
+    return PosStyles(
+      align: align,
+      bold: bold,
+      height: size,
+      width: size,
+      fontType: font,
+    );
+  }
+
   Future<List<int>> _printElement(Generator generator, Map<String, dynamic> element, Map<String, dynamic> order, POSController posController, PrinterModel printer, String? title) async {
     List<int> bytes = [];
     final type = element['type'];
+    final styles = _getStyles(element);
 
     switch (type) {
       case 'HEADER':
       case 'STORE_NAME':
-        bytes += generator.text(_normalizeString(posController.restaurantName.value), styles: const PosStyles(align: PosAlign.center, bold: true, height: PosTextSize.size2, width: PosTextSize.size2));
-        if (posController.restaurantAddress.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantAddress.value), styles: const PosStyles(align: PosAlign.center));
-        if (posController.restaurantPhone.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantPhone.value), styles: const PosStyles(align: PosAlign.center));
+        bytes += generator.text(_normalizeString(posController.restaurantName.value), styles: _getStyles(element, defaultBold: true, defaultSize: PosTextSize.size2));
+        if (posController.restaurantAddress.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantAddress.value), styles: _getStyles(element));
+        if (posController.restaurantPhone.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantPhone.value), styles: _getStyles(element));
         break;
       case 'LOGO':
       case 'CAFE_LOGO':
@@ -112,45 +140,45 @@ class PrinterService {
             if (image != null) {
               final maxWidth = printer.paperSize == '58mm' ? 150 : 200;
               img.Image resized = img.copyResize(image, width: maxWidth);
-              bytes += generator.image(resized);
+              bytes += generator.image(resized, align: styles.align);
             }
           } catch (e) {}
         }
         break;
       case 'STORE_ADDRESS':
-        if (posController.restaurantAddress.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantAddress.value), styles: const PosStyles(align: PosAlign.center));
+        if (posController.restaurantAddress.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantAddress.value), styles: styles);
         break;
       case 'STORE_PHONE':
-        if (posController.restaurantPhone.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantPhone.value), styles: const PosStyles(align: PosAlign.center));
+        if (posController.restaurantPhone.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.restaurantPhone.value), styles: styles);
         break;
       case 'ORDER_INFO':
-        if (title != null) bytes += generator.text(_normalizeString(title.toUpperCase()), styles: const PosStyles(align: PosAlign.center, bold: true));
-        bytes += generator.text(_normalizeString('ID: ${order['id']}'), styles: const PosStyles(align: PosAlign.center));
-        bytes += generator.text(_normalizeString(DateFormat('dd.MM.yyyy HH:mm').format(DateTime.now())), styles: const PosStyles(align: PosAlign.center));
+        if (title != null) bytes += generator.text(_normalizeString(title.toUpperCase()), styles: _getStyles(element, defaultBold: true));
+        bytes += generator.text(_normalizeString('ID: ${order['id']}'), styles: styles);
+        bytes += generator.text(_normalizeString(DateFormat('dd.MM.yyyy HH:mm').format(DateTime.now())), styles: styles);
         if (order['table'] != null && order['table'] != '-') {
-            bytes += generator.text(_normalizeString('STOL: ${order['table']}'), styles: const PosStyles(align: PosAlign.center, bold: true, height: PosTextSize.size2, width: PosTextSize.size2));
+            bytes += generator.text(_normalizeString('STOL: ${order['table']}'), styles: _getStyles(element, defaultBold: true, defaultSize: PosTextSize.size2));
         }
         break;
       case 'ITEMS_TABLE':
         bytes += generator.hr(ch: '-');
         bytes += generator.row([
-          PosColumn(text: _normalizeString('NOMI'), width: 7, styles: const PosStyles(bold: true)),
-          PosColumn(text: _normalizeString('SONI'), width: 2, styles: const PosStyles(bold: true, align: PosAlign.center)),
-          PosColumn(text: _normalizeString('NARXI'), width: 3, styles: const PosStyles(bold: true, align: PosAlign.right)),
+          PosColumn(text: _normalizeString('NOMI'), width: 7, styles: styles.copyWith(bold: true)),
+          PosColumn(text: _normalizeString('SONI'), width: 2, styles: styles.copyWith(bold: true, align: PosAlign.center)),
+          PosColumn(text: _normalizeString('NARXI'), width: 3, styles: styles.copyWith(bold: true, align: PosAlign.right)),
         ]);
         bytes += generator.hr(ch: '-');
         final items = order['details'] as List;
         for (var item in items) {
           bytes += generator.row([
-            PosColumn(text: _normalizeString(item['name']), width: 7),
-            PosColumn(text: _normalizeString(item['qty'].toString()), width: 2, styles: const PosStyles(align: PosAlign.center)),
-            PosColumn(text: _normalizeString(_formatPrice((item['price'] as num) * (item['qty'] as num))), width: 3, styles: const PosStyles(align: PosAlign.right)),
+            PosColumn(text: _normalizeString(item['name']), width: 7, styles: styles),
+            PosColumn(text: _normalizeString(item['qty'].toString()), width: 2, styles: styles.copyWith(align: PosAlign.center)),
+            PosColumn(text: _normalizeString(_formatPrice((item['price'] as num) * (item['qty'] as num))), width: 3, styles: styles.copyWith(align: PosAlign.right)),
           ]);
         }
         bytes += generator.hr(ch: '-');
         break;
       case 'TOTAL_BLOCK':
-        bytes += _row(generator, 'JAMI:', _formatPrice(order['total']), bold: true);
+        bytes += _row(generator, 'JAMI:', _formatPrice(order['total']), styles: styles.copyWith(bold: true));
         break;
       case 'DIVIDER':
         bytes += generator.hr(ch: '-');
@@ -162,32 +190,38 @@ class PrinterService {
         String link = posController.instagramLink.value;
         if (link.isEmpty && posController.instagram.value.isNotEmpty) link = "https://instagram.com/${posController.instagram.value.replaceAll('@', '')}";
         if (link.isNotEmpty) {
-           bytes += generator.text(_normalizeString('INSTAGRAM'), styles: const PosStyles(align: PosAlign.center, bold: true));
-           bytes += generator.qrcode(link, size: QRSize.size4);
+           bytes += generator.text(_normalizeString('INSTAGRAM'), styles: styles.copyWith(bold: true));
+           bytes += generator.qrcode(link, size: _getQRSize(element['props']?['size']), align: styles.align);
         }
         break;
       case 'TELEGRAM_QR':
         String link = posController.telegramLink.value;
         if (link.isEmpty && posController.telegram.value.isNotEmpty) link = "https://t.me/${posController.telegram.value.replaceAll('t.me/', '')}";
         if (link.isNotEmpty) {
-           bytes += generator.text(_normalizeString('TELEGRAM'), styles: const PosStyles(align: PosAlign.center, bold: true));
-           bytes += generator.qrcode(link, size: QRSize.size4);
+           bytes += generator.text(_normalizeString('TELEGRAM'), styles: styles.copyWith(bold: true));
+           bytes += generator.qrcode(link, size: _getQRSize(element['props']?['size']), align: styles.align);
         }
         break;
       case 'FOOTER':
-        if (posController.receiptFooter.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.receiptFooter.value), styles: const PosStyles(align: PosAlign.center, bold: true));
+        if (posController.receiptFooter.value.isNotEmpty) bytes += generator.text(_normalizeString(posController.receiptFooter.value), styles: styles.copyWith(bold: true));
         break;
       case 'WIFI_INFO':
         if (posController.wifiSsid.value.isNotEmpty) {
-          bytes += generator.text(_normalizeString('Wi-Fi: ${posController.wifiSsid.value}'), styles: const PosStyles(align: PosAlign.center));
-          bytes += generator.text(_normalizeString('Parol: ${posController.wifiPassword.value}'), styles: const PosStyles(align: PosAlign.center));
+          bytes += generator.text(_normalizeString('Wi-Fi: ${posController.wifiSsid.value}'), styles: styles);
+          bytes += generator.text(_normalizeString('Parol: ${posController.wifiPassword.value}'), styles: styles);
         }
         break;
       case 'KITCHEN_TITLE':
-        bytes += generator.text(_normalizeString(element['props']?['title'] ?? '*** OSHXONA ***'), styles: const PosStyles(align: PosAlign.center, bold: true, height: PosTextSize.size2, width: PosTextSize.size2));
+        bytes += generator.text(_normalizeString(element['props']?['title'] ?? '*** OSHXONA ***'), styles: _getStyles(element, defaultBold: true, defaultSize: PosTextSize.size2));
         break;
     }
     return bytes;
+  }
+
+  QRSize _getQRSize(String? size) {
+    if (size == 'LARGE') return QRSize.size5;
+    if (size == 'XLARGE') return QRSize.size6;
+    return QRSize.size4;
   }
 
   List<int> _printSideBySide(Generator generator, Map<String, dynamic> elL, Map<String, dynamic> elR, POSController pos) {
@@ -197,16 +231,21 @@ class PrinterService {
       if (el['type'] == 'WIFI_INFO') return 'WI-FI';
       return el['label'] ?? "";
     }
+
+    final stylesL = _getStyles(elL);
+    final stylesR = _getStyles(elR);
+
     return generator.row([
-      PosColumn(text: _normalizeString(getLabel(elL)), width: 6, styles: const PosStyles(align: PosAlign.center, bold: true)),
-      PosColumn(text: _normalizeString(getLabel(elR)), width: 6, styles: const PosStyles(align: PosAlign.center, bold: true)),
+      PosColumn(text: _normalizeString(getLabel(elL)), width: 6, styles: stylesL.copyWith(align: PosAlign.center, bold: true)),
+      PosColumn(text: _normalizeString(getLabel(elR)), width: 6, styles: stylesR.copyWith(align: PosAlign.center, bold: true)),
     ]);
   }
 
-  List<int> _row(Generator g, String left, String right, {bool bold = false}) {
+  List<int> _row(Generator g, String left, String right, {PosStyles? styles}) {
+    final s = styles ?? const PosStyles();
     return g.row([
-      PosColumn(text: _normalizeString(left), width: 7, styles: PosStyles(bold: bold)),
-      PosColumn(text: _normalizeString(right), width: 5, styles: PosStyles(align: PosAlign.right, bold: bold)),
+      PosColumn(text: _normalizeString(left), width: 7, styles: s),
+      PosColumn(text: _normalizeString(right), width: 5, styles: s.copyWith(align: PosAlign.right)),
     ]);
   }
 
